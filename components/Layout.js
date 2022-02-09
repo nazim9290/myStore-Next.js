bb7a00edbc51d602aee4bb64471a967ebef9cd50
Nazim Uddin <nushourav4@gmail.com>
2022-02-10 05:08:37 +0900

    fetching data

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,7 @@ const Layout = ({ children }) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
         />
+        <link rel="stylesheet" href="style.css" />
       </Head>
       <Navbar />
       {children}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
 import Navbar from "../components/Navbar";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>hello Next js</h1>
-    </div>
-  );
+const Home = ({ products }) => {
+  const productList = products.map((product) => {
+    return (
+      <div className="card" key={product._id}>
+        <div className="card-image">
+          <img src={product.mediaUrl} />
+          <span className="card-title black-text">{product.name}</span>
+          <a className="btn-floating halfway-fab waves-effect waves-light red">
+            <i className="material-icons">add</i>
+          </a>
+        </div>
+        <div className="card-content">
+          <p>{product.description}</p>
+          <h4>{product.price}</h4>
+        </div>
+      </div>
+    );
+  });
+  // console.log(products);
+  return <div className="rootcard">{productList}</div>;
 };
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const data = await res.json();
+  return {
+    props: {
+      products: data,
+    },
+  };
+}
 
 export default Home;
